345ae73888a355337fcc7e5454e72e654eb08e73
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-10-16 15:13:58 -0700

    lab4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,9 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Hangman
+// An implementation of this game
+// Karyssa Krebs <kkrebs1@ucsc.edu>
+// Oct 16
 
-// Constants
 
-// Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -15,7 +14,47 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+
 }
 
 // let's get this party started
 main();
+
+
+// LAB 3 PSUEDOCODE
+
+// 1. One person picks a word
+//      Input number of lines for the amount of letters in the word
+// 2. Guessers will choose a letter to guess
+//      They have 6 guesses before the hangman is fully drawn 
+// 3. The letters that are correct are written on the specific line where the letter is supposed to be
+// 4. The letters that are incorrect are written on the side
+//      A part of the body of the hangman will be drawn
+// 5. Guessers will keep guessing till their guesses are up or they figure out what the word is
+//      If guessers know the word then they say it and if correct they win the game
+//          If the guess of the word is wrong, they lose the game
+
+
+// Take input_string from user1 
+//    Loop through input_string, increase count by 1 for every letter string contains (input_string = cat, count = 3)
+//        Print new string “_” for number in count (count = 3, new_string = “_ _ _”)
+// Loop until input_string = new_string
+//    Take input_char from user2
+//        Iterate through input_string
+//            If input_char is in input_string
+//                Update input_char at index in new_string
+//                Print new string (char = c, new_string = “c _ _”)
+//            If input_char is not in input_string
+//                Increase strike_count by 1
+//                Print updated hangman
+//                If strike_ count = 6
+//                    Print player lost message
+//                    Close program
+// Print player won message
+// Close program
+
+
+
+
+
+
