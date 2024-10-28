099575a8d631ece25a75bbe7a69d14d9ee4dc668
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-10-28 12:53:54 -0700

    lecture practice

--- lecture/js/lecture.js
+++ lecture/js/lecture.js
@@ -1,10 +1,27 @@
-
-
 // VARIABLE ASSIGNMENT
-
 let my_num = 10;
 
 
 // CONSTANT (VARS THAT STAY THE SAME)
-
 const int = 5;
+
+
+// FUNCTION STRUCTURE
+function myFunction(parameter) {
+     //some code here
+     return value;
+}
+
+function reverseSortArray(array) {
+	// sort array
+  let sortedArray = array.sort();
+  // reverse array
+  let reverseSortedArray = sortedArray.reverse();
+  // return array
+  return reverseSortedArray;
+}
+
+const mixedArray = ["olives", "pickles", "bone broth", "salt lick", 6, 
+	"salted carmel"];
+
+reverseSortArray(mixedArray);
