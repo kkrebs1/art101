6b9168195c0b73e1bc3b2e822bcf90dac0699f6b
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-10-28 14:21:15 -0700

    updated lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,21 +1,20 @@
 // Author: Karyssa Krebs
 // Date: Oct 28, 2024
 
-
-function sort_name() {
-  var user_name = window.prompt("Please enter your name.");
-  var sort_user_name = user_name.split('').sort().join().replace(/,/g, '');
-  return sort_user_name;
-
-}
-new_name = sort_name();
-document.getElementById("sorted_output").innerText = "Your sorted name: " + new_name;
-
-
-
-
 function main() {
   console.log("Main function started.");
 }
 
 main();
+
+
+function sort_name() {
+  var user_name = window.prompt("Please enter your name.");
+  console.log("user_name =", user_name)
+  var sort_user_name = user_name.split('').sort().join('');
+  return sort_user_name;
+
+}
+
+document.writeln("Your sorted name: ",
+  sort_name(), "</br>");
