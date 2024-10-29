7de97632f251718b460401c220f46bae29756dcd
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-10-28 17:17:54 -0700

    finished lab

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -9,12 +9,15 @@ main();
 
 
 function sort_name() {
+  //prompt user to input name
   var user_name = window.prompt("Please enter your name.");
-  console.log("user_name =", user_name)
-  var sort_user_name = user_name.split('').sort().join('');
-  return sort_user_name;
+  //print name to console
+  console.log("user_name =", user_name);
+  //splits string into an array, sorts array, and joins it back to string
+  var sort_user_name = user_name.toLowerCase().split('').sort().join('');
+  //return sorted user name
+  return sort_user_name.charAt(0).toUpperCase() + sort_user_name.slice(1);
 
 }
-
-document.writeln("Your sorted name: ",
-  sort_name(), "</br>");
+//call fuction and write new name to html file
+document.writeln(sort_name(), "</br>");
