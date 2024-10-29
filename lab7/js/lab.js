4a2de7fbd080df8daa444dfd099f2d5daf208a9a
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-10-28 21:45:23 -0700

    task x stuff

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,21 +3,28 @@
 
 function main() {
   console.log("Main function started.");
+  //prompt user to input name
+  var user_name = window.prompt("Please enter your name.");
+  //call fuction and write new name to html file
+  document.writeln(sort_name(user_name), "</br>");
 }
 
 main();
 
 
-function sort_name() {
+function sort_name(user_name) {
   //prompt user to input name
-  var user_name = window.prompt("Please enter your name.");
+  //var user_name = window.prompt("Please enter your name.");
   //print name to console
   console.log("user_name =", user_name);
+  //omits whitespace from user input
+  var no_space = user_name.split(' ').join(''); 
+  console.log(no_space);
   //splits string into an array, sorts array, and joins it back to string
-  var sort_user_name = user_name.toLowerCase().split('').sort().join('');
+  var sort_user_name = no_space.toLowerCase().split('').sort().join('');
   //return sorted user name
   return sort_user_name.charAt(0).toUpperCase() + sort_user_name.slice(1);
 
 }
 //call fuction and write new name to html file
-document.writeln(sort_name(), "</br>");
+//document.writeln(sort_name(), "</br>");
