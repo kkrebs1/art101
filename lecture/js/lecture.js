fdb13291cccbd1ad7da0fb4622e100ae24274d30
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-10-30 15:24:09 -0700

    lecture

--- lecture/js/lecture.js
+++ lecture/js/lecture.js
@@ -1,10 +1,10 @@
 // VARIABLE ASSIGNMENT
 let my_num = 10;
 
-
 // CONSTANT (VARS THAT STAY THE SAME)
 const int = 5;
 
+// _________________________________________________________________________________________________________
 
 // FUNCTION STRUCTURE
 function myFunction(parameter) {
@@ -25,3 +25,30 @@ const mixedArray = ["olives", "pickles", "bone broth", "salt lick", 6,
 	"salted carmel"];
 
 reverseSortArray(mixedArray);
+
+// _________________________________________________________________________________________________________
+
+// CALLBACK FUNCTIONS: A callback is code that responds to an event
+
+function pressed() {
+  //respond to button
+  //display something or do something else
+}
+
+
+
+// example of using a callback
+function allDone() {
+  console.log("I'm dont waiting!");
+}
+setTimeout(allDone, 5000);
+//setTimeout(callback(as a reference), time);
+
+
+
+
+function spiceItUp(str) {
+  console.log(str + " but spicy!");
+}
+var myArray = ["tomatoes", "persimmons", "pumpkin spiced latte"];
+myArray.map(spiceItUp);
