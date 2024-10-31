f29151d9faa1678cac60775791aaa12993053f2b
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-10-30 23:47:55 -0700

    lab8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -22,6 +22,17 @@ var result2 = array.map(function (x) {
 
 console.log("Subtracting 1234 to each of the numbers in my array: ", result2);
 
+//your map results
+var map_results = "Your map results here";
+
+//use jQuery to select the elements by its ID and set the HTML content
+$("#add1").html("Calculate 5 + 1234" + add(5));
+$("#add2").html("Calculate 256 + 1234" + add(256));
+$("#my_array").html("My array: " + array);
+$("#add3").html("Adding 1234 to each of the numbers in my array: " + result);
+$("#subtract").html("Subtracting 1234 to each of the numbers in my array: "+ result2);
+
+
 function main() {
 }
 
