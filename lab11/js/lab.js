5e2c5cdf0eab2d8b9e39399537805584167c9853
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-11-11 19:40:15 -0800

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,16 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+//  lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version
+// Author: Karyssa Krebs
+// Date: Nov 11 2024
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// Sorts the characters of a string in alphabetical order.
+function sortString(inputString) {
+  // We have to convert our string to an array and back again to sort it
+  return inputString.split('').sort().join('');
 }
+
+// click listener for button
+$("#submit").click(function () {
+  // get value of input field
+  const userName = $("#user-name").val();
+  // now let's sort it
+  userNameSorted = sortString(userName);
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
+});
+
 
 function main() {
   console.log("Main function started.");
