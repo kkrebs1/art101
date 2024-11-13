ea94f09ef6061c2b2fa5497793363ce81f0ab2b2
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-11-13 15:21:58 -0800

    homepage

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,16 +1,32 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Karyssa Krebs
+// Date: Nov 12
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  let houseArray = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"];
+  if (mod == 1) {
+    return houseArray[0];
+    
+  }
+  else if (mod == 2) {
+    return houseArray[1];
+  }
+  else if (mod == 3) {
+    return houseArray[2];
+  }
+  else if (mod == 4){
+    return houseArray[3];
+  }
 }
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  newText = "<p> The Sorting Hat has sorted you into " + house + ".<p>";
+  document.getElementById("output").innerHTML = newText;
+})
 
 function main() {
   console.log("Main function started.");
