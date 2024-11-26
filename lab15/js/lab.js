f470f3f5535b1a39630dbb1100924f0186296e43
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-11-25 17:21:38 -0800

    lab15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,21 +1,35 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Karyssa Krebs
+// Date: 11/25/24
 
-// Constants
+// Using the core $.ajax() method
+$.ajax({
+  // The URL for the request (from the api docs)
+  url: "http://www.yerkee.com/api/fortune/science",
+  // The data to send (will be converted to a query string)
+  data: {
+    "fortune": "I do not remember ever having seen a sustained argument by an author which,\nstarting from philosophical premises likely to meet with general acceptance,\nreached the conclusion that a praiseworthy ordering of one's life is to\ndevote it to research in mathematics.\n\t\t-- Sir Edmund Whittaker, \"Scientific American\", Vol. 183"
+  },
+  // Whether this is a POST or GET request
+  type: "GET",
+  // The type of data we expect back
+  dataType : "json",
+  // What do we do when the api call is successful
+  //   all the action goes in here
+  success: function(data) {
+      // do stuff
+      console.log(data);
+  },
+  // What we do if the api call fails
+  error: function (jqXHR, textStatus, errorThrown) { 
+      // do stuff
+      console.log("Error:", textStatus, errorThrown);
+  }
+})
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
 }
 
-// let's get this party started
 main();
