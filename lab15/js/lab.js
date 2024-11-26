2738a9ab31dae06bd9b006220d6530b303a45d35
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-11-25 17:27:25 -0800

    test

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -2,30 +2,29 @@
 // Author: Karyssa Krebs
 // Date: 11/25/24
 
-// Using the core $.ajax() method
+
 $.ajax({
   // The URL for the request (from the api docs)
-  url: "http://www.yerkee.com/api/fortune/science",
-  // The data to send (will be converted to a query string)
-  data: {
-    "fortune": "I do not remember ever having seen a sustained argument by an author which,\nstarting from philosophical premises likely to meet with general acceptance,\nreached the conclusion that a praiseworthy ordering of one's life is to\ndevote it to research in mathematics.\n\t\t-- Sir Edmund Whittaker, \"Scientific American\", Vol. 183"
-  },
+  url: "http://www.yerkee.com/api/fortune",
+  // The data to send (this API does not require additional parameters)
+  data: {}, 
   // Whether this is a POST or GET request
-  type: "GET /api/fortune/science",
+  type: "GET",
   // The type of data we expect back
   dataType : "json",
-  // What do we do when the api call is successful
-  //   all the action goes in here
+  // What do we do when the API call is successful
+  // All the action goes in here
   success: function(data) {
-      // do stuff
-      console.log(data);
+      // Log the fortune to the console
+      console.log("Fortune:", data.fortune);
   },
-  // What we do if the api call fails
+  // What we do if the API call fails
   error: function (jqXHR, textStatus, errorThrown) { 
-      // do stuff
+      // Log the error details
       console.log("Error:", textStatus, errorThrown);
   }
-})
+});
+
 
 
 function main() {
