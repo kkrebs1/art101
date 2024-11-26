9a38148c10a99f1318ce0ba91b63787599eb4a3a
kkrebs1 <124312861+kkrebs1@users.noreply.github.com>
2024-11-25 17:36:55 -0800

    test

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -2,29 +2,29 @@
 // Author: Karyssa Krebs
 // Date: 11/25/24
 
-
+// Using the core $.ajax() method
 $.ajax({
   // The URL for the request (from the api docs)
   url: "http://www.yerkee.com/api/fortune",
-  // The data to send (this API does not require additional parameters)
-  data: {}, 
+  // The data to send (will be converted to a query string)
+  data: {},
   // Whether this is a POST or GET request
   type: "GET",
   // The type of data we expect back
   dataType : "json",
-  // What do we do when the API call is successful
-  // All the action goes in here
+  // What do we do when the api call is successful
+  //   all the action goes in here
   success: function(data) {
-      // Log the fortune to the console
-      console.log("Fortune:", data.fortune);
+      // do stuff
+      console.log("Sucess!");
+      $("#output").append("<p>" + data.fortune + "</p>");
   },
-  // What we do if the API call fails
+  // What we do if the api call fails
   error: function (jqXHR, textStatus, errorThrown) { 
-      // Log the error details
+      // do stuff
       console.log("Error:", textStatus, errorThrown);
   }
-});
-
+})
 
 
 function main() {
